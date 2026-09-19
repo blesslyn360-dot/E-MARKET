a7395480df7727f9b7d79692632b432d51ba6e44
Blesslyn Akorful <blesslyn360@gmail.com>
2026-09-19 19:05:22 +0000

    fix trust proxy plcement

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const companyRoutes = require('./routes/company');
 const configRoutes = require('./routes/config');
 
 const app = express();
+ app.set('trust proxy',1);
 const isProduction = process.env.NODE_ENV === 'production';
 
 // Connect to MongoDB
@@ -65,5 +66,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
- const app = express();
- app.set('trust proxy',1);
